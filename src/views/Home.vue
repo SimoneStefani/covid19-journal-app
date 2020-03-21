84ba656595475ed49485613cc139e4d1990f1544
Daynex <deanr@live.de>
2020-03-21 10:02:25 +0100

    Add date

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,21 @@
   <div class="home">
     <h2 v-if="!user">Nobody is authenticated</h2>
     <h2 v-else>Welcome {{ user.email }}</h2>
+    <h3>Today is: {{ today.getDate() + '.' + today.getMonth() + '.' + today.getFullYear() }}</h3>
 
     <div>
-      <p>{{question_list[current_quuestion_index]}}</p>
-      <input v-model="answer_list[current_quuestion_index]" />
-      <p>Answer: {{ answer_list[current_quuestion_index]}}</p>
+      <p>{{question_list[current_question_index]}}</p>
+      <input v-model="answer_list[current_question_index]" />
+      <p>Answer: {{ answer_list[current_question_index]}}</p>
     </div>
-    <button v-if="current_quuestion_index > 0" @click="current_quuestion_index--">Previous</button>
+    <button v-if="current_question_index > 0" @click="current_question_index--">Previous</button>
     <button
-      v-if="current_quuestion_index < question_list.length - 1"
-      @click="current_quuestion_index++"
+      v-if="current_question_index < question_list.length - 1"
+      @click="current_question_index++"
     >Next</button>
+    <div v-else>
+      <button @click="handleSubmit">Submit</button>
+    </div>
 
     <div v-if="user" style="margin-top: 50px">
       <button @click="handleLogout">Logout</button>
@@ -29,13 +33,14 @@ export default {
   data() {
     return {
       user: null,
+      today: new Date(),
       question_list: [
         "Are you feeling sick?",
         "Do you have a fever?",
         "Do you have trouble breathing?"
       ],
       answer_list: [],
-      current_quuestion_index: 0
+      current_question_index: 0
     };
   },
 
@@ -44,6 +49,13 @@ export default {
   },
 
   methods: {
+    handleSubmit() {
+      this.question_list.forEach((element, i) => {
+        console.log(element);
+        console.log(this.answer_list[i] + "\n");
+      });
+    },
+
     handleLogout() {
       firebase
         .auth()
